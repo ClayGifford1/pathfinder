755ea96f7850463b3c576dfb89056c18bbe2a501
Clay Gifford <claygifford1@gmail.com>
2021-03-19 18:37:29 -0500

    refactored grid function

---
+++ cell.js
@@ -0,0 +1,21 @@
+export default class Node {
+  constructor(row, cell) {
+    this.row = row;
+    this.cell = cell;
+    this.isBase = false;
+    this.isTarget = false;
+    this.isObstacle = false;
+    this.isVisited = false;
+  }
+
+  createNode() {
+    const newNode = document.createElement("td");
+    newNode.className = "cell";
+    newNode.id = `r${this.row}c${this.cell}`;
+
+    const text = document.createTextNode("");
+    newNode.appendChild(text);
+
+    return newNode;
+  }
+}
--- generateGrid.js
+++ generateGrid.js
@@ -1,4 +1,6 @@
-function generateGrid() {
+import Node from './cell.js';
+
+const generateGrid = () => {
   const grid = document.getElementById("graph");
   const nav = document.getElementById("navigation");
   const info = document.getElementById("info");
@@ -6,32 +8,31 @@ function generateGrid() {
   const infoHeight = info.offsetHeight;
   const sizes = calculateGridSize();
   const rows = (sizes.height - navHeight - infoHeight) / 27;
-  const cells = sizes.width / 27;
+  const cells = sizes.width / 25;
 
   for (let x = 0; x < rows; x++) {
     var row = document.createElement("tr");
-    row.class = "gridRow";
     for (let y = 0; y < cells; y++) {
-      var cell = document.createElement("td");
-      cell.class = "cell";
-      var text = document.createTextNode("");
-      cell.appendChild(text);
+      var node = new Node(x, y);
+      var cell = node.createNode();
       row.appendChild(cell);
     }
     grid.appendChild(row);
   }
 }
 
-function clearGrid() {
+const clearGrid = () => {
   const grid = document.getElementById("graph");
   while(grid.hasChildNodes()) {
     grid.removeChild(grid.firstChild);
   }
 }
 
-function calculateGridSize() {
+const calculateGridSize = () => {
   const sizes = {
     height: window.innerHeight,
     width: window.innerWidth };
   return sizes;
 }
+
+generateGrid();

06ef1555065cc27a8eb369c8625133e3cd69f940
Clay Gifford <claygifford1@gmail.com>
2021-06-03 10:48:55 -0500

    refactored

--- eucledian.js
+++
@@ -1,9 +0,0 @@
-export default function eucledian(node, grid) {
-
-  let x = grid.targetNode.cell - node.cell;
-  let y = grid.targetNode.row - node.row;
-
-  let distance = Math.sqrt((x * x) + (y * y));
-
-  return distance;
-}
--- manhattan.js
+++
@@ -1,9 +0,0 @@
-export default function manhattan(node, grid) {
-
-  let x = Math.abs(grid.targetNode.cell - node.cell);
-  let y = Math.abs(grid.targetNode.row - node.row);
-  
-  let distance = x + y;
-
-  return distance;
-}
